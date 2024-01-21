547fcce053e942d92dec8cb2dd1015acf3e2f409
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2024-01-21 14:03:13 -0300

    Starting to create unit tests

--- src/test/java/org/example/costbudgeting/CostBudgetingApplicationTests.java
+++ src/test/java/org/example/costbudgeting/CostBudgetingApplicationTests.java
@@ -2,12 +2,15 @@ package org.example.costbudgeting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class CostBudgetingApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ src/test/java/org/example/costbudgeting/services/impl/RawMaterialServiceImplTest.java
@@ -0,0 +1,36 @@
+package org.example.costbudgeting.services.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RawMaterialServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createNewRawMaterial() {
+    }
+
+    @Test
+    void deleteRawMaterial() {
+    }
+
+    @Test
+    void updateRawMaterial() {
+    }
+
+    @Test
+    void getRawMaterial() {
+    }
+}
